3db3007394de3f2a7bb03a4fe52e4020590f9e69
Tamicktom <rickpokemon38@gmail.com>
2026-08-22 16:00:35 -0300

    now ballons spawns

--- scenes/Ballon_manager.cs
+++ scenes/Ballon_manager.cs
@@ -1,5 +1,8 @@
+//* Libraries imports */
 using Godot;
 using System;
+
+//* Local imports */
 using Ballonpopper.scripts;
 
 namespace Ballonpopper.scenes;
@@ -8,26 +11,43 @@ public partial class Ballon_manager : Node3D
 {
   [Export(Godot.PropertyHint.Range, "1, 10, 1")]
   public int ballon_count = 1;
+  [Export(Godot.PropertyHint.Range, "0.1, 10, 0.1")]
+  public float minimal_spawn_delay = 0.2f;
+  [Export(Godot.PropertyHint.Range, "0.1, 10, 0.1")]
+  public float max_spawn_delay = 2.0f;
+  [Export(Godot.PropertyHint.ResourceType, "Scene")]
+  public Godot.PackedScene BallonScene; // modelo do balão
+
   public int score = 0;
 
   public Godot.Label ui_points;
+  private Godot.Timer spawn_timer;
 
   // Called when the node enters the scene tree for the first time.
   public override void _Ready()
   {
 
-	foreach (var child in this.GetChildren())
-	{
-	  if (child is Balloon balloon)
-	  {
-		balloon.Popped += this.IncreaseScore;
-	  }
+    foreach (var child in this.GetChildren())
+    {
+      if (child is Balloon balloon)
+      {
+        balloon.Popped += this.IncreaseScore;
+      }
 
-	  if (child is Godot.Label label)
-	  {
-		this.ui_points = label;
-	  }
-	}
+      if (child is Godot.Label label)
+      {
+        this.ui_points = label;
+      }
+    }
+
+    this.spawn_timer = new Godot.Timer
+    {
+      WaitTime = GD.RandRange(this.minimal_spawn_delay, this.max_spawn_delay),
+      Autostart = true,
+    };
+
+    this.AddChild(this.spawn_timer);
+    this.spawn_timer.Timeout += this.SpawnBallon;
   }
 
   // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -37,8 +57,27 @@ public partial class Ballon_manager : Node3D
 
   public void IncreaseScore(int amount)
   {
-	this.score += amount;
-	Godot.GD.Print(score);
-	this.ui_points.Text = $"Points: {this.score}";
+    this.score += amount;
+    this.ui_points.Text = $"Points: {this.score}";
+  }
+
+  private void SpawnBallon()
+  {
+    if (this.BallonScene == null) return;
+
+    var balloon = this.BallonScene.Instantiate<Balloon>();
+
+    double randomX = GD.RandRange(-5.0, 5.0);
+    double randomY = GD.RandRange(-0, 3.0);
+    double randomZ = GD.RandRange(-2.0, 2.0);
+
+    balloon.Position = new Godot.Vector3(
+      (float)randomX,
+      (float)randomY,
+      (float)randomZ
+    );
+
+    balloon.Popped += IncreaseScore;
+    this.AddChild(balloon);
   }
 }
--- scripts/Balloon.cs
+++ scripts/Balloon.cs
@@ -14,6 +14,8 @@ public partial class Balloon : Area3D
   public float SizeIncreaseFactor = 1.2f;
   [Godot.Export(Godot.PropertyHint.Range, "0.1,1,0.1")] // 0.1 to 1, step 0.1
   public float PopAnimationDuration = 0.25f;
+
+  // * Private Variables
   protected bool _canBePopped = true;
   protected Godot.AudioStream _popSound;
   protected Godot.AudioStream _inflateSound;
@@ -24,8 +26,10 @@ public partial class Balloon : Area3D
   // Called when the node enters the scene tree for the first time.
   public override void _Ready()
   {
-	this._popSound = Godot.GD.Load<Godot.AudioStream>("res://assets/ballon_pop_sound_effect.ogg");
-	this._inflateSound = Godot.GD.Load<Godot.AudioStream>("res://assets/balloon_inflating.ogg");
+    this._popSound = Godot.GD.Load<Godot.AudioStream>("res://assets/ballon_pop_sound_effect.ogg");
+    this._inflateSound = Godot.GD.Load<Godot.AudioStream>("res://assets/balloon_inflating.ogg");
+
+    this.StartFloatingUp();
   }
 
   // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -35,92 +39,107 @@ public partial class Balloon : Area3D
 
   public override void _InputEvent(Godot.Camera3D camera, Godot.InputEvent @event, Godot.Vector3 eventPosition, Godot.Vector3 normal, int shapeIdx)
   {
-	var isMouseEvent = @event is Godot.InputEventMouseButton;
-	if (isMouseEvent)
-	{
-	  // print the event
-	  if (IsLeftClickDown(@event as Godot.InputEventMouseButton) && this._canBePopped)
-	  {
-		this.ClicksToPop--;
-		if (this.ClicksToPop <= 0)
-		{
-		  this._canBePopped = false;
-		  this.PopBalloonAnimation();
-		}
-		else
-		{
-		  this.IncreaseBalloonSize();
-		}
-	  }
-	}
+    var isMouseEvent = @event is Godot.InputEventMouseButton;
+    if (isMouseEvent)
+    {
+      // print the event
+      if (IsLeftClickDown(@event as Godot.InputEventMouseButton) && this._canBePopped)
+      {
+        this.ClicksToPop--;
+        if (this.ClicksToPop <= 0)
+        {
+          this._canBePopped = false;
+          this.PopBalloonAnimation();
+        }
+        else
+        {
+          this.IncreaseBalloonSize();
+        }
+      }
+    }
   }
 
   private static bool IsLeftClickDown(Godot.InputEventMouseButton mouseButtonEvent)
   {
-	return mouseButtonEvent.ButtonIndex == Godot.MouseButton.Left && mouseButtonEvent.IsPressed();
+    return mouseButtonEvent.ButtonIndex == Godot.MouseButton.Left && mouseButtonEvent.IsPressed();
   }
 
   private void IncreaseBalloonSize()
   {
-	var actualScale = this.Scale;
-	var finalScale = new Godot.Vector3(
-	  this.SizeIncreaseFactor * actualScale.X,
-	  this.SizeIncreaseFactor * actualScale.Y,
-	  this.SizeIncreaseFactor * actualScale.Z
-	);
-	var duration = this.PopAnimationDuration;
+    var actualScale = this.Scale;
+    var finalScale = new Godot.Vector3(
+      this.SizeIncreaseFactor * actualScale.X,
+      this.SizeIncreaseFactor * actualScale.Y,
+      this.SizeIncreaseFactor * actualScale.Z
+    );
+    var duration = this.PopAnimationDuration;
 
-	this.IncreaseBalloonSizeTween(finalScale, duration);
-	this.PlayInflateSound();
+    this.IncreaseBalloonSizeTween(finalScale, duration);
+    this.PlayInflateSound();
   }
 
   private async void PopBalloonAnimation()
   {
-	var actualScale = this.Scale;
-	var finalScale = new Godot.Vector3(
-	  this.SizeIncreaseFactor * actualScale.X,
-	  this.SizeIncreaseFactor * actualScale.Y,
-	  this.SizeIncreaseFactor * actualScale.Z
-	);
-	var duration = this.PopAnimationDuration;
+    var actualScale = this.Scale;
+    var finalScale = new Godot.Vector3(
+      this.SizeIncreaseFactor * actualScale.X,
+      this.SizeIncreaseFactor * actualScale.Y,
+      this.SizeIncreaseFactor * actualScale.Z
+    );
+    var duration = this.PopAnimationDuration;
 
-	await this.ToSignal(this.IncreaseBalloonSizeTween(finalScale, duration), Tween.SignalName.Finished);
-	this.EmitSignal(SignalName.Popped, this.PointsToGive);
-	this.PlayPopSound();
-	this.QueueFree();
+    await this.ToSignal(this.IncreaseBalloonSizeTween(finalScale, duration), Tween.SignalName.Finished);
+    this.EmitSignal(SignalName.Popped, this.PointsToGive);
+    this.PlayPopSound();
+    this.QueueFree();
   }
 
   private void PlayPopSound()
   {
-	var soundPlayer = new Godot.AudioStreamPlayer
-	{
-	  Stream = this._popSound,
-	};
-	this.GetParent().AddChild(soundPlayer);
-	soundPlayer.Finished += soundPlayer.QueueFree;
-	soundPlayer.Play();
+    var soundPlayer = new Godot.AudioStreamPlayer
+    {
+      Stream = this._popSound,
+    };
+    this.GetParent().AddChild(soundPlayer);
+    soundPlayer.Finished += soundPlayer.QueueFree;
+    soundPlayer.Play();
   }
 
   private void PlayInflateSound()
   {
-	var soundPlayer = new Godot.AudioStreamPlayer
-	{
-	  Stream = this._inflateSound,
-	};
-	this.GetParent().AddChild(soundPlayer);
-	soundPlayer.Finished += soundPlayer.QueueFree;
-	soundPlayer.Play();
+    var soundPlayer = new Godot.AudioStreamPlayer
+    {
+      Stream = this._inflateSound,
+    };
+    this.GetParent().AddChild(soundPlayer);
+    soundPlayer.Finished += soundPlayer.QueueFree;
+    soundPlayer.Play();
   }
 
-  private Tween IncreaseBalloonSizeTween(Vector3 finalScale, float duration)
+  private Godot.Tween IncreaseBalloonSizeTween(Vector3 finalScale, float duration)
   {
-	var property = Node3D.PropertyName.Scale.ToString();
+    var property = Node3D.PropertyName.Scale.ToString();
 
-	var tween = this.CreateTween();
-	tween.TweenProperty(this, property, finalScale, duration)
-	  .SetTrans(Tween.TransitionType.Sine)
-	  .SetEase(Tween.EaseType.Out);
+    var tween = this.CreateTween();
+    tween.TweenProperty(this, property, finalScale, duration)
+      .SetTrans(Godot.Tween.TransitionType.Sine)
+      .SetEase(Godot.Tween.EaseType.Out);
 
-	return tween;
+    return tween;
+  }
+
+  private Godot.Tween StartFloatingUp()
+  {
+    string property = Node3D.PropertyName.Position.ToString();
+    Godot.Vector3 finalPosition = new Godot.Vector3(this.Position.X, this.Position.Y * 2.5f, this.Position.Z);
+    float randomDuration = GD.RandRange(1, 5);
+    float duration = 2.5f * randomDuration;
+
+    var tween = this.CreateTween();
+    tween.TweenProperty(this, property, finalPosition, duration)
+      .SetTrans(Godot.Tween.TransitionType.Sine)
+      .SetEase(Godot.Tween.EaseType.Out);
+
+    return tween;
   }
 }
